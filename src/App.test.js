f74eab20542696867e9a9ad11208be238b4c7282
Edward Mendoza <lalo.garibo90@gmail.com>
2020-08-19 14:41:53 -0700

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Trying again", () => {
+  const { getByText } = render(<App />);
+  const pTag = getByText(/Trying again/);
+  expect(pTag).toBeInTheDocument();
+});
+
+test("renders user name", () => {
+  const { getByText } = render(<App />);
+  const name = getByText(/Eduardo/);
+  expect(name).toBeInTheDocument();
+});
+
+test("renders user name", () => {
+  const { getByText } = render(<App />);
+  const name = getByText(/Eduardo/);
+  expect(name).toBeInTheDocument();
 });
